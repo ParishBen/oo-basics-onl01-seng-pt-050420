ccdac1599d2657fccd5be914a43e882175e00dd8
Ben Parish <parishben13@gmail.com>
2020-05-21 02:41:43 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -6,9 +6,9 @@ def title
   @title
  end
  attr_accessor :author, :page_count, :genre
- def turn_page(page_count)
+ def turn_page
    @page_count = 0
-   @page_count+=page_count
+   @page_count+=1
    
  end
 end
